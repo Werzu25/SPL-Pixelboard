a518612e2e6c939eb86679e9b8f899893eaaa1e3
Werzu25 <leonw@astecs.com>
2024-10-02 15:26:37 +0200

    added methods refresh isPressed and wasPressed in button.cpp

--- src/button.cpp
+++ src/button.cpp
@@ -1,17 +1,42 @@
-class button
-{
+#include <Arduino.h>
+
+class DebouncedButton {
 private:
-    bool buttonStatus;
-    
+    uint8_t pin;
+    unsigned long lastDebounceTime;
+    unsigned long debounceDelay;
+    bool lastButtonState;
+    bool buttonState;
+    bool lastPressState;
+
 public:
-    button(/* args */);
-    ~button();
+    DebouncedButton(uint8_t buttonPin, unsigned long debounceDelayMs = 50)
+        : pin(buttonPin), debounceDelay(debounceDelayMs), lastDebounceTime(0), lastButtonState(HIGH), buttonState(HIGH), lastPressState(false) {
+        pinMode(pin, INPUT_PULLUP);
+    }
+
+    void refresh() {
+        bool reading = digitalRead(pin);
+        if (reading != lastButtonState) {
+            lastDebounceTime = millis();
+        }
+
+        if ((millis() - lastDebounceTime) > debounceDelay) {
+            if (reading != buttonState) {
+                buttonState = reading;
+            }
+        }
+
+        lastButtonState = reading;
+    }
+
+    bool isPressed() const {
+        return !buttonState;
+    }
+
+    bool wasPressed() {
+        bool wasPressed = (buttonState == LOW && lastPressState == false);
+        lastPressState = (buttonState == LOW);
+        return wasPressed;
+    }
 };
-
-button::button(/* args */)
-{
-}
-
-button::~button()
-{
-}
