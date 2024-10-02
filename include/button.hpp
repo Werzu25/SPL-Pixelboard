d92378fe506eef44a9c7f10e18862cecbb12750a
Werzu25 <leonw@astecs.com>
2024-10-02 16:38:10 +0200

    added button.hpp header file

--- include/button.hpp
+++ include/button.hpp
@@ -1,6 +1,24 @@
-#ifndef BUTTON_H
-#define BUTTON_H
+#ifndef BUTTON_HPP
+#define BUTTON_HPP
 
+#include <Arduino.h>
 
+class EntprellterTaster {
+private:
+    uint8_t pin;
+    unsigned long letzteEntprellZeit;
+    unsigned long entprellVerzoegerung;
+    bool letzterTasterZustand;
+    bool tasterZustand;
+    bool letzterDruckZustand;
 
-#endif
+public:
+    EntprellterTaster(uint8_t tasterPin, unsigned long entprellVerzoegerungMs = 50);
+
+    void aktualisieren();
+    bool istLangGedrueckt(unsigned long langDruckDauer);
+    bool istGedrueckt() const;
+    bool wurdeGedrueckt();
+};
+
+#endif // BUTTON_HPP
