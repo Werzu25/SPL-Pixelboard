32653df3685ccc60dee82ea12deaa912a6692617
junox0103 <julian.raffeiner@gmail.com>
2025-01-22 14:40:19 +0100

    julian get cold

---
+++ src/joystick.cpp
@@ -0,0 +1,30 @@
+#ifndef JOYSTICK_HPP
+#define JOYSTICK_HPP
+
+#include <Arduino.h>
+#include "button.hpp"
+
+class Joystick : public EntprellterTaster {
+private:
+    int pinX;
+    int pinY;
+
+public:
+    // Constructor
+    Joystick(int pinButton, int pinX, int pinY) : EntprellterTaster(pinButton), pinX(pinX), pinY(pinY) {
+        pinMode(pinX, INPUT);
+        pinMode(pinY, INPUT);
+    }
+
+    // Get the X position of the joystick
+    int getX() {
+        return analogRead(pinX);
+    }
+
+    // Get the Y position of the joystick
+    int getY() {
+        return analogRead(pinY);
+    }
+};
+
+#endif // JOYSTICK_HPP
